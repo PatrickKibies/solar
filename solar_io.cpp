82d131ce8effd49b472def79f7df08b82a856d5a
Patrick Kibies <patrick.kibies@tu-dortmund.de>
2016-02-26 19:49:22 +0100

    Added some files to handle IO, nothing finished by now

---
+++ solar_io.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+# include <string>
+# include <sstream>
+# include <vector>
+std::string write_dcd_line(std::vector <arma::vec> particle_positions){
+  
+  int i_particleCount = particle_positions.size();
+  
+  
+  for (int i = 0; i<i_particleCount -1; i++){
+    
+    //Do the formatting stuff here    
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  return "This function is not fully implemented now!";
+}
---
+++ solar_io.h
@@ -0,0 +1,7 @@
+# include <iostream>
+# include <string>
+# include <sstream>
+# include <vector>
+
+std::string write_dcd_line(std::vector <arma::vec> particle_positions);
+
